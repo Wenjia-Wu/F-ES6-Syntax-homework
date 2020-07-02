80eb5def2994634b8693568c6ed9a7ba5bb4ccd3
wenjia.wu <wenjia.wu@thoughtworks.com>
2020-07-02 22:44:30 +0800

    finish practice1 and practice2.

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
-export { parseData };
+export {parseData};
+
+const parseData = input => {
+    const {data, column} = input;
+    const key = column.map(x => x.name);
+    return data.map(function (x) {
+        let output = {};
+        for (const [index, element] of key.entries()) {
+            output[element] = x[index]
+        }
+        return output;
+    })
+}
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,9 @@
-const inject
-export { inject };
+export {inject};
+
+const inject = (items, sections) => {
+    const inputItems = items.concat();
+    for (let element of sections) {
+        items.splice(items.indexOf(inputItems[element.index]), 0, element.content)
+    }
+    return items;
+}
